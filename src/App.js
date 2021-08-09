7ce0264071c5e54f5ef0e4a538341de17eef3b60
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-08-08 23:45:18 -0300

    parte 2 feito

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Route path='/about' component={About} />
-        <Route path='/users' component={Users} />
+        <Route path='/users/:id' render={(props) => <Users {...props} greetingsMessage="Good Morning"/>} />
         <Route exact path='/' component={Home}/>
       </BrowserRouter>  
     );
